c03f02910cd03a5d29df10cd7399309fb2665494
alicephilippa <alice.p.chapman@gmail.com>
2021-07-21 11:00:36 +0100

    Update pins_FLSUN_HISPEED.h

--- Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
+++ Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
@@ -275,10 +275,10 @@
 // TFT with FSMC interface
 //
 
-#if HAS_FSMC_TFT || HAS_GRAPHICAL_TFT
-  #define TFT_CS_PIN                        PD7   // NE4
-  #define TFT_RS_PIN                        PD11  // A0
-#endif
+//#if HAS_FSMC_TFT || HAS_GRAPHICAL_TFT
+//  #define TFT_CS_PIN                        PD7   // NE4
+//  #define TFT_RS_PIN                        PD11  // A0
+//#endif
 
 #if HAS_FSMC_TFT
   /**
