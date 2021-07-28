c738eb9fe0e5b58852b21b3696b1b022d07a4a73
alicephilippa <alice.p.chapman@gmail.com>
2021-07-28 19:18:55 +0100

    Change pins_FLSUN_HIGHSPEED.h
    Fix TFT_xx_PIN assignmet

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2715,7 +2715,7 @@
 // 320x240, 2.8", FSMC Display From MKS
 // Normally used in MKS Robin Nano V1.2
 //
-//#define MKS_ROBIN_TFT28
+#define MKS_ROBIN_TFT28
 
 //
 // 320x240, 3.2", FSMC Display From MKS
@@ -2738,7 +2738,7 @@
 // 320x240, 3.2", FSMC Display From MKS
 // Normally used in MKS Robin
 //
-#define MKS_ROBIN_TFT_V1_1R
+// #define MKS_ROBIN_TFT_V1_1R
 
 //
 // 480x320, 3.5", FSMC Stock Display from TronxXY
